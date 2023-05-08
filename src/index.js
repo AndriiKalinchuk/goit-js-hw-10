71f58acd09cea656cd33ef8ca0919c3ae33d8335
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-08 19:48:40 +0300

    folder-js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
